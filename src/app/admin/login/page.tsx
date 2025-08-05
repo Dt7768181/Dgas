694dc276a9c20bb66627b105b556dfdc59d0f830
Dheeraj Thummalapenta <dt7768181@gmail.com>
2025-08-05 08:44:19 +0000

    the fields are email and password

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -42,7 +42,12 @@ export default function AdminLoginPage() {
               />
             </div>
             <div className="grid gap-2">
-                <Label htmlFor="password">Password</Label>
+                <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <Link href="#" className="ml-auto inline-block text-sm underline">
+                        Forgot your password?
+                    </Link>
+                </div>
                 <Input 
                     id="password" 
                     type="password" 
