89ec5d60780673253e725e6acaf4891ea83846d0
Dheeraj Thummalapenta <dt7768181@gmail.com>
2025-08-05 06:26:25 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="hidden

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 
 import { BookingForm } from '@/components/booking-form';
-import { GasCylinderIcon } from '@/components/icons/gas-cylinder-icon';
+import { HeroPattern } from '@/components/hero-pattern';
 
 export default function Home() {
   return (
@@ -13,8 +13,8 @@ export default function Home() {
           <p className="max-w-[600px] text-lg text-muted-foreground md:text-xl lg:mx-0">
             Get your gas cylinders delivered to your doorstep with ease. Schedule your delivery in just a few clicks.
           </p>
-          <div className="hidden lg:flex justify-center items-center rounded-lg bg-secondary/50 p-8 shadow-inner">
-              <GasCylinderIcon className="w-64 h-64 text-primary opacity-50" />
+          <div className="hidden lg:block">
+            <HeroPattern />
           </div>
         </div>
         <div className="flex justify-center">
